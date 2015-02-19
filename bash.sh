8e14c856c8b0676b9133246c8d6b0477350f0739
J.D. Grimes <jdg@efieldguide.com>
2015-02-19 16:05:33 -0500

    Echo file name

--- bash.sh
+++ bash.sh
@@ -5,7 +5,7 @@
         	local GITHUB_SRC=${1}_GITHUB_SRC
         	local GITHUB_TREE=${1}_GITHUB_TREE
         	local DIR=${1}_DIR
-        
+        echo "https://github.com/${!GITHUB_SRC}/archive/${!GITHUB_TREE}.tar.gz"
             mkdir -p "${!DIR}"
             curl -L "https://github.com/${!GITHUB_SRC}/archive/${!GITHUB_TREE}.tar.gz" \
                 | tar xvz --strip-components=1 -C "${!DIR}"
