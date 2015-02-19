a5178bf176c81a5c570d32712431b766a2a6cdde
J.D. Grimes <jdg@efieldguide.com>
2015-02-19 16:08:24 -0500

    s/GITHUB_TREE/GIT_TREE

--- bash.sh
+++ bash.sh
@@ -3,10 +3,10 @@
         install-from-github() {
         
         	local GITHUB_SRC=${1}_GITHUB_SRC
-        	local GITHUB_TREE=${1}_GITHUB_TREE
+        	local GIT_TREE=${1}_GIT_TREE
         	local DIR=${1}_DIR
         echo "https://github.com/${!GITHUB_SRC}/archive/${!GITHUB_TREE}.tar.gz"
             mkdir -p "${!DIR}"
-            curl -L "https://github.com/${!GITHUB_SRC}/archive/${!GITHUB_TREE}.tar.gz" \
+            curl -L "https://github.com/${!GITHUB_SRC}/archive/${!GIT_TREE}.tar.gz" \
                 | tar xvz --strip-components=1 -C "${!DIR}"
         }
