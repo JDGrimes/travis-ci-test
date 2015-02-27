a565a607a6e5d30b99a4270b9df71dd59618a31f
J.D. Grimes <jdg@efieldguide.com>
2015-02-27 17:50:11 -0500

    mkdir

--- bash.sh
+++ bash.sh
@@ -4,7 +4,7 @@ set -e
 shopt -s expand_aliases
 
 # Install CubePoints when running tests.
-	mkdir -p /tmp/cubepoints
+	mkdir -p /tmp/wordpress/wp-content/plugins/cubepoints
 	curl -s https://downloads.wordpress.org/plugin/cubepoints.3.2.1.zip > /tmp/cubepoints.zip
 	unzip /tmp/cubepoints.zip -d /tmp/wordpress/wp-content/plugins/
 
