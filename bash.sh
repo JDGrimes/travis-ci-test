595b1265a38b520ba7fc1c43829658ef9551f79a
J.D. Grimes <jdg@efieldguide.com>
2015-04-30 12:41:48 -0400

    wp git

--- bash.sh
+++ bash.sh
@@ -3,12 +3,25 @@
 set -e
 shopt -s expand_aliases
 
-# Install CubePoints when running tests.
-	mkdir -p /tmp/wordpress/wp-content/plugins/cubepoints
-	curl -s https://downloads.wordpress.org/plugin/cubepoints.3.2.1.zip > /tmp/cubepoints.zip
-	unzip /tmp/cubepoints.zip -d /tmp/wordpress/wp-content/plugins/
+	WP_DEVELOP_DIR=/tmp/wordpress
+	WP_VERSION=4.0
+	mkdir -p "WP_DEVELOP_DIR"
 
-	ls /tmp/wordpress/wp-content/plugins/cubepoints/
+	# Clone the WordPress develop repo.
+	git clone --depth=1 --branch="$WP_VERSION" git://develop.git.wordpress.org/ "WP_DEVELOP_DIR"
 
+	# Set up tests config.
+	cd "WP_DEVELOP_DIR"
+	cp wp-tests-config-sample.php wp-tests-config.php
+	sed -i "s/youremptytestdbnamehere/wordpress_test/" wp-tests-config.php
+	sed -i "s/yourusernamehere/root/" wp-tests-config.php
+	sed -i "s/yourpasswordhere//" wp-tests-config.php
+	cd -
 
+	# Set up database.
+ls "WP_DEVELOP_DIR"
+ls "WP_DEVELOP_DIR"/tests
+ls "WP_DEVELOP_DIR"/tests/phpunit/
+ls "WP_DEVELOP_DIR"/tests/phpunit/includes
+ 
 set +e
