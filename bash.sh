a1e9f35858de6ce6c7b3136a774d34152ea02b6e
J.D. Grimes <jdg@efieldguide.com>
2015-04-30 14:50:39 -0400

    sed

--- bash.sh
+++ bash.sh
@@ -4,7 +4,8 @@ set -e
 shopt -s expand_aliases
 
 	WP_DEVELOP_DIR=/tmp/wordpress
-	WP_VERSION=4.0
+WP_TESTS_DIR=/tmp/wordpress/tests/phpunit
+WP_VERSION=4.0
 	mkdir -p "WP_DEVELOP_DIR"
 
 	# Clone the WordPress develop repo.
@@ -17,6 +18,8 @@ shopt -s expand_aliases
 	sed -i "s/yourusernamehere/root/" wp-tests-config.php
 	sed -i "s/yourpasswordhere//" wp-tests-config.php
 	cd -
+		sed -i 's/do_action( '"'"'admin_init'"'"' )/if ( ! isset( $GLOBALS['"'"'_did_admin_init'"'"'] ) \&\& $GLOBALS['"'"'_did_admin_init'"'"'] = true ) do_action( '"'"'admin_init'"'"' )/' \
+			"$WP_TESTS_DIR"/includes/testcase-ajax.php
 
 	# Set up database.
 ls "WP_DEVELOP_DIR"
